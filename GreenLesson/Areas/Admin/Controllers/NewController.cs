6bf08891e22334005c7431f0e69c2d8476d88afd
nguyen long <longnguyenhoang26@gmail.com>
2020-07-21 19:05:21 +0700

    add finder vào prroẹct

---
+++ GreenLesson/Areas/Admin/Controllers/NewController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GreenLesson.Areas.Admin.Controllers
+{
+    public class NewController : BaseController
+    {
+        // GET: Admin/New
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
--- Model/EF/New.cs
+++ Model/EF/New.cs
@@ -1,4 +1,4 @@
-namespace Model.EF
+﻿namespace Model.EF
 {
     using System;
     using System.Collections.Generic;
@@ -13,27 +13,35 @@ namespace Model.EF
         public long ID { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Tên tin tức")]
         public string Name { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Mô tả")]
         public string Description { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Meta Title")]
         public string MetaTitle { get; set; }
 
+        [Display(Name = "Hình ảnh")]
         public string Image { get; set; }
 
+        [Display(Name = "Người tạo")]
         public long? CategoryID { get; set; }
 
         [Column(TypeName = "ntext")]
+        [Display(Name = "Nôi dung")]
         public string Content { get; set; }
 
         [StringLength(250)]
         public string MetaDescriptions { get; set; }
 
+        [Display(Name = "Ngày tạo")]
         public DateTime? CreatedDate { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Tên đăng nhập")]
         public string ModifiedBy { get; set; }
 
         [StringLength(50)]
@@ -43,6 +51,7 @@ namespace Model.EF
 
         public DateTime? ModifiedDate { get; set; }
 
+        [Display(Name = "Trạng thái")]
         public bool? Status { get; set; }
 
         [StringLength(50)]
