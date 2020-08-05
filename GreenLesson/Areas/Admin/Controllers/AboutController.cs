5015395ea8f69ce0d2c91889dde5de4e02ff8b10
nguyen long <longnguyenhoang26@gmail.com>
2020-08-05 15:22:36 +0700

    Thực hiện chức năng giới thiệu doanh nghiệp

---
+++ GreenLesson/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,106 @@
+﻿using GreenLesson.Common;
+using Model.Dao;
+using Model.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using System.Net;
+
+namespace GreenLesson.Areas.Admin.Controllers
+{
+    public class AboutController : Controller
+    {
+        private GreenLessonDbContext db = new GreenLessonDbContext();
+        // GET: Admin/About
+        public ActionResult Index(string searchString, int page = 1, int pageSize = 5)
+        {
+            var dao = new AboutDao();
+            var model = dao.ListAllPaging(searchString, page, pageSize);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            SetViewBag();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Create([Bind(Include = "ID,Name,Description,MetaTitle,Image,UserBy,Detail,Status")] About abouts)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Abounts.Add(abouts);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            SetViewBag();
+            return View(abouts);
+        }
+
+        public void SetViewBag(long? selectedId = null)
+        {
+            var dao = new UserDao();
+            ViewBag.UserBy = new SelectList(dao.ListAll(), "Name", "Name", selectedId);
+
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var abouts = new AboutDao().ViewDetail(id);
+            return View(abouts);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ValidateInput(false)]
+        public ActionResult Edit(About abouts)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new AboutDao();
+
+                var result = dao.Update(abouts);
+                if (result)
+                {
+                    //SetAlert("Cập nhật thành công", "success");
+                    return RedirectToAction("Index", "About");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Cập nhật không thành công");
+                }
+            }
+            return View("Index");
+        }
+
+       
+
+        [HttpDelete]
+        public ActionResult Delete(int id)
+        {
+            new AboutDao().Delete(id);
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            About abouts = db.Abounts.Find(id);
+            if (abouts == null)
+            {
+                return HttpNotFound();
+            }
+            return View(abouts);
+        }
+    }
+}
--- Model/Dao/AboutDao.cs
+++ Model/Dao/AboutDao.cs
@@ -1,4 +1,5 @@
 ﻿using Model.EF;
+using PagedList;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,9 +16,67 @@ namespace Model.Dao
             db = new GreenLessonDbContext();
         }
 
+        public bool Update(About entity)
+        {
+            try
+            {
+                var abouts = db.Abounts.Find(entity.ID);
+                abouts.Name = entity.Name;
+                if (!string.IsNullOrEmpty(entity.Name))
+                {
+                    abouts.Name = entity.Name;
+                }
+                abouts.Description = entity.Description;
+                abouts.MetaTitle = entity.MetaTitle;
+                abouts.Image = entity.Image;
+                abouts.Detail = entity.Detail;
+                abouts.Status = entity.Status;
+                abouts.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public IEnumerable<About> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<About> model = db.Abounts;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Name.Contains(searchString) || x.Description.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+        //public New GetById(string Name)
+        //{
+        //    return db.Abounts.SingleOrDefault(x => x.Name == Name);
+        //}
+
+        public List<About> ListAll()
+        {
+            return db.Abounts.Where(x => x.Status == true).Take(4).ToList();
+        }
+
         public About ViewDetail(long id)
         {
             return db.Abounts.Find(id);
+        }
+
+        public bool Delete(int id)
+        {
+            try
+            {
+                var abouts = db.Abounts.Find(id);
+                db.Abounts.Remove(abouts);
+                db.SaveChanges();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
         }
     }
 }
